9dd1351b9930c820b9bb24e4ccc91c6b8b7675ba
sriram_srinivas <sriram_s@soalrtis.com>
2021-05-17 16:07:28 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Speed 1thz")
+console.log(
+
+);
+const [mMass, mHeight] = prompt("Mark's Weight, Height").split(',');
+const [jMass, jHeight] = prompt("John's Weight, Height").split(',');
+const mBMI = mMass / (mHeight ** 2);
+const jBMI = jMass / (jHeight ** 2);
+const markHigherBMI = mBMI > jBMI;
+alert(`Mark has Higher BMI: ${markHigherBMI} 🚗`);
