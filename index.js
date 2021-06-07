5ee12600477372ee9ba86c2c14e486fecb6615e1
sriram_srinivas <sriram_s@soalrtis.com>
2021-06-07 10:40:25 +0530

    progress

--- index.js
+++ index.js
@@ -4,24 +4,12 @@ const Person = {
     Year:1994,
     calcAge:function(year){
         console.log("func",this);
+        const test =  () => {
+            console.log("inner func",this);
+        }
+        test()
         return year - this.Year
-    },
-    calcAge1: year => {
-        console.log("arrow",this);
-        year - this.Year
     }
 }
 
-function calcAge (year){
-    console.log("func def",this);
-}
-const calcAge1 =  year => {
-    console.log("arrow",this);
-}
-
-const calcAge2 = function (year) {
-    console.log("func dec",this);
-}
-
-
-console.log(calcAge(2021),calcAge1(2021),calcAge2(2021),Person.calcAge(2021),Person.calcAge1(2021));
+Person.calcAge(2004)
