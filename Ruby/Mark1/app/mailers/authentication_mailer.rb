abc485fcc26f97f78802bd0ffb59ac802c3a270d
srirams-fw <sriram.s@freshworks.com>
2024-08-15 08:08:45 +0530

    moved to separate repo

--- Ruby/Mark1/app/mailers/authentication_mailer.rb
+++
@@ -1,9 +0,0 @@
-class AuthenticationMailer < ApplicationMailer
-    default from: 'sriram.rsrc@gmail.com' # Set a default sender email
-
-  def auth_email(user)
-    #@user = user
-    @user = user
-    mail(to: @user, subject: 'Login to FreshIMS')
-  end
-end
--- Ruby/Mark1/test/mailers/previews/authentication_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/authentication_mailer
-class AuthenticationMailerPreview < ActionMailer::Preview
-
-end
