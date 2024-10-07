d05bc42221905caf2330e84e9597bd03239e2fb6
Sriram S <sriram.s@freshworks.com>
2024-10-07 19:33:30 +0530

    ember utils

---
+++ FW Utils/emberPathToRoute.js
@@ -0,0 +1,21 @@
+const pathToComponent = input => {
+    const output = input.split("/").filter(name => !name.includes(".")).map(
+        path => path.split("-").map(
+            subPath => subPath[0].toUpperCase()+subPath.slice(1)).join("")).join("::")
+        return {input,output}
+}
+
+const input = "frontend/app/ui-components/module-tickets/list/template.hbs";
+
+console.log(pathToComponent(input))
+
+const componentToPath = input => {
+    const output = input.split("::").map(
+        part => part.replace(/([A-Z])/g, "-$1").toLowerCase().slice(1) // Add hyphen before uppercase and convert to lowercase
+    ).join("/");
+    return {input, output};
+};
+
+// Example usage:
+const input1 = "Frontend::App::UiComponents::ModuleTickets::List";
+console.log(componentToPath(input1));
